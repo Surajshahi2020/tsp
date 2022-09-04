0d1b32766b18db6ac7d6b29d482464db4fb17c3e
Suraj shahi <80339587+Surajshahi2020@users.noreply.github.com>
2022-09-04 21:55:20 +0545

    Update nearest.php

--- map/nearest.php
+++ map/nearest.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Google Map</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="http://localhost/project/map/tourist.php">Located Places</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="http://localhost/project/map/tourist.php">Home</a></li>
+      <li><a href="http://localhost/project/map/user-map.php">Locate</a></li>
+      <li>
+
+
 <?php
 include('db.php');
 
@@ -109,4 +132,3 @@ if($query->rowCount() > 0)
 
 
 ?>
-
